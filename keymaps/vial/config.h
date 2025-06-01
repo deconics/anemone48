f1eda5d53e5fac733d139e003bd4c1a35ccabdd4
deconics <164635544+deconics@users.noreply.github.com>
2025-06-01 18:09:04 +0900

    Delete keymaps/vial directory

--- keymaps/vial/config.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2025 deconics (@deconics)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x91, 0x99, 0x11, 0x4A, 0x3E, 0xF9, 0xCC, 0xBE}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
-#define TAPPING_TERM 200
